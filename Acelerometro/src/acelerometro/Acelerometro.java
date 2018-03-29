4ca361233840ae37b2551aab1da6b27d8308146f
selennemm99 <selennemm99@gmail.com>
2018-03-29 15:42:01 -0500

    c:

---
+++ Acelerometro/src/acelerometro/Acelerometro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acelerometro;
+
+/**
+ *
+ * @author selen
+ */
+public class Acelerometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmAcelerometro frmAcelerometro = new FrmAcelerometro ();
+        frmAcelerometro.setVisible(true);
+    }
+    
+}
